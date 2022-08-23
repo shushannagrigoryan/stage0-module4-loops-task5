1c006135b080c24de1759189079f38fc360515b2
Shushanna <shushannagrigoryan7@gmail.com>
2022-08-23 18:21:38 +0400

    8

--- src/main/java/school/mjc/stage0/loops/task5/Triangle.java
+++ src/main/java/school/mjc/stage0/loops/task5/Triangle.java
@@ -4,16 +4,17 @@ public class Triangle {
     public void printTriangle(int cathetusLength) {
         for(int i = 0;i<cathetusLength; i++){
             for(int j = 0;j<cathetusLength;j++){
-                if(i == 0 || i == cathetusLength-1){
-                    System.out.print('8');
-                }
-                else if(j == 0 || j == cathetusLength-1){
+                if(i == cathetusLength - 1){
                     System.out.print('8');
                 }
                 else{
-                    System.out.print(" ");
+                    if(j == 0 || j == i){
+                        System.out.print('8');
+                    }
+                    else{
+                        System.out.print(" ");
+                    }
                 }
-
             }
             System.out.println("");
         }
