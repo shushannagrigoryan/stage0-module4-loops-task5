93499c4bf37c02e9befc6a25d21090da9e1344e3
Shushanna <shushannagrigoryan7@gmail.com>
2022-08-23 18:04:42 +0400

    6

--- src/main/java/school/mjc/stage0/loops/task5/Cross.java
+++ src/main/java/school/mjc/stage0/loops/task5/Cross.java
@@ -4,10 +4,14 @@ public class Cross {
     public void printCross(int sideLength) {
         for(int i = 0;i<sideLength; i++){
             for(int j = 0;j<sideLength;j++){
-                if(j == i || j == sideLength-i-1){
+                if(i == sideLength / 2){
                     System.out.print('8');
                 }
 
+                else if(j == sideLength / 2){
+
+                    System.out.print('8');
+                }
                 else{
                     System.out.print(" ");
                 }
